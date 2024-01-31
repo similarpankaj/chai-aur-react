635f6ca37b5cfffb7c7bb4ad76617c627f8e4f04
Pankaj <pankaj.upadhyay269@gmail.com>
2024-01-31 17:08:30 +0530

    Hello world

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
+import Chai from "./chai"
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <h1>
-      Hello react with vite | PU
-    </h1>
+    <>
+    <Chai/>
+    <h1>chai aur react</h1>
+    </>
   )
 }
 
